602f9f6dcd5036f9ad6381898d194f77fc5ba3c1
Guillermo Rodas <garethderioth@gmail.com>
2018-11-26 11:00:22 -0500

    Verificando nuestro JWT firmado

--- api/index.js
+++ api/index.js
@@ -14,6 +14,17 @@ app.post("/api/auth/token", function(req, res) {
   res.json({ access_token: token });
 });
 
+app.get("/api/auth/verify", function(req, res, next) {
+  const { access_token } = req.query;
+
+  try {
+    const decoded = jwt.verify(access_token, config.authJwtSecret);
+    res.json({ message: "the access token is valid", username: decoded.sub });
+  } catch (err) {
+    next(err);
+  }
+});
+
 const server = app.listen(5000, function() {
   console.log(`Listening http://localhost:${server.address().port}`);
 });
