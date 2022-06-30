f59526e85199ea30976f30d7b958542ba330c114
manumac86 <emmartinez86@gmail.com>
2022-06-30 13:09:33 +0200

    Setup and update

--- api/index.js
+++ api/index.js
@@ -1,15 +1,14 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const jwt = require("jsonwebtoken");
-const request = require("request");
-const { config } = require("./config");
+const express = require('express');
+const jwt = require('jsonwebtoken');
+const request = require('request');
+const { config } = require('./config');
 
-const encodeBasic = require("./utils/encodeBasic");
+const encodeBasic = require('./utils/encodeBasic');
+const { json } = require('express');
 
 const app = express();
 
-// body parser
-app.use(bodyParser.json());
+app.use(json());
 
 function getUserPlaylists(accessToken, userId) {
   if (!accessToken || !userId) {
@@ -19,11 +18,11 @@ function getUserPlaylists(accessToken, userId) {
   const options = {
     url: `https://api.spotify.com/v1/users/${userId}/playlists`,
     headers: { Authorization: `Bearer ${accessToken}` },
-    json: true
+    json: true,
   };
 
   return new Promise((resolve, reject) => {
-    request.get(options, function(error, response, body) {
+    request.get(options, function (error, response, body) {
       if (error || response.statusCode !== 200) {
         reject(error);
       }
@@ -33,41 +32,41 @@ function getUserPlaylists(accessToken, userId) {
   });
 }
 
-app.post("/api/auth/token", function(req, res) {
+app.post('/api/auth/token', function (req, res) {
   const { email, username, name } = req.body;
   const token = jwt.sign({ sub: username, email, name }, config.authJwtSecret);
   res.json({ access_token: token });
 });
 
-app.get("/api/auth/verify", function(req, res, next) {
+app.get('/api/auth/verify', function (req, res, next) {
   const { access_token } = req.query;
 
   try {
     const decoded = jwt.verify(access_token, config.authJwtSecret);
-    res.json({ message: "the access token is valid", username: decoded.sub });
+    res.json({ message: 'the access token is valid', username: decoded.sub });
   } catch (err) {
     next(err);
   }
 });
 
-app.get("/api/playlists", async function(req, res, next) {
+app.get('/api/playlists', async function (req, res, next) {
   const { userId } = req.query;
 
   const authOptions = {
-    url: "https://accounts.spotify.com/api/token",
+    url: 'https://accounts.spotify.com/api/token',
     headers: {
       Authorization: `Basic ${encodeBasic(
         config.spotifyClientId,
         config.spotifyClientSecret
-      )}`
+      )}`,
     },
     form: {
-      grant_type: "client_credentials"
+      grant_type: 'client_credentials',
     },
-    json: true
+    json: true,
   };
 
-  request.post(authOptions, async function(error, response, body) {
+  request.post(authOptions, async function (error, response, body) {
     if (error || response.statusCode !== 200) {
       next(error);
     }
@@ -78,6 +77,6 @@ app.get("/api/playlists", async function(req, res, next) {
   });
 });
 
-const server = app.listen(5000, function() {
+const server = app.listen(5000, function () {
   console.log(`Listening http://localhost:${server.address().port}`);
 });
